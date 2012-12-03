2c1f15530345bc30ce4e681e3176738ef8b792c3
Amit Singh <AmitS@glam.com>
2012-12-03 16:14:34 +0530

    Added the project...

---
+++ MapApplication/MapApplication/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MapApplication
+//
+//  Created by Amit Singh on 03/12/12.
+//  Copyright (c) 2012 Amit Singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MapApplication/MapApplication/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MapApplication
+//
+//  Created by Amit Singh on 03/12/12.
+//  Copyright (c) 2012 Amit Singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
