be7d2dda39ec58384c40cdde776d0c5fffeaa8cf
Amit Singh <AmitS@glam.com>
2012-12-03 17:48:48 +0530

    Added main view controller..

---
+++ MapApplication/MapApplication/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  MapApplication
+//
+//  Created by Amit Singh on 03/12/12.
+//  Copyright (c) 2012 Amit Singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
